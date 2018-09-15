21b86c5ea21d799aece44929b0c3b69304227a0e
Patrick LUNGU <plungu@e3r7p11.42.fr>
2018-09-15 17:20:29 +0200

    ft_dimensions 2e jet + header *2 & malloc

--- ex00/ft_dimensions.c
+++ ex00/ft_dimensions.c
@@ -6,10 +6,11 @@
 /*   By: plungu <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/15 16:57:30 by plungu            #+#    #+#             */
-/*   Updated: 2018/09/15 17:17:10 by plungu           ###   ########.fr       */
+/*   Updated: 2018/09/15 17:20:13 by plungu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "RUSH02_H"
 
 struct	s_dim	ft_dimensions(t_list *list)
@@ -18,6 +19,8 @@ struct	s_dim	ft_dimensions(t_list *list)
 	t_char	*elem;
 
 	elem = list->first;
+	if (!(dim = malloc(sizeof(dim))))
+		return (0);
 	dim.x = 0;
 	dim.y = 0;
 
